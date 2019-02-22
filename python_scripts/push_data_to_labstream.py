2eeaa2010dab7ebb91634b02d15d729015109b06
marc101101 <progamer101101@gmx.de>
2019-02-22 14:37:42 +0100

    Updated script

--- python_scripts/push_data_to_labstream.py
+++ python_scripts/push_data_to_labstream.py
@@ -1,12 +1,48 @@
 import sys
 import time
+from pylsl import StreamInfo, StreamOutlet
+
+info = StreamInfo(client_name, 'FD', 17, 100, 'float32', 'gum11127_Openface')
+outlet = StreamOutlet(info)
 k = 0
+
+#std::cout << face_id << ", "
+#		<< frame_number << ", " 
+#		<< timestamp << ", " 
+#		<< landmark_detection_success << ", " 
+#		<< landmark_detection_confidence << ", " 
+#		<< gaze_direction0.x << ", " 
+#		<< gaze_direction0.y << ", " 
+#		<< gaze_direction0.z << ", " 
+#		<< gaze_direction1.x << ", "
+#		<< gaze_direction1.y << ", "
+#		<< gaze_direction1.z << ", "
+#		<< head_pose[0] << ", "
+#		<< head_pose[1] << ", "
+#		<< head_pose[2] << ", "
+#		<< head_pose[3] << ", "
+#		<< head_pose[4] << ", "
+#		<< head_pose[5] << std::endl;
+
 try:
     buff = ''
     while True:
         buff += sys.stdin.read(1)
         if buff.endswith('\n'):
-            print("this is python log: "  +  buff[:-1])
+            print("Message received: "  +  str(buff[:-1].split(",")))
+            frame_to_push = buff[:-1].split(",")
+            counter = 0
+            for i in frame_to_push:
+            	try:
+            		frame_to_push[counter] = float(i)
+            		pass
+            	except Exception as e:
+            		frame_to_push[counter] = 0
+            		raise e
+            	
+            	counter = counter+1
+            print(frame_to_push)
+            outlet.push_sample(frame_to_push)
             buff = ''
             k = k + 1
 except KeyboardInterrupt:
