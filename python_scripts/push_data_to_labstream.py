a910e66258501111c9c708b1fc3b0b73a390eadf
Markus Guder <progamer101101@gmx.de>
2019-02-22 17:40:45 +0100

    fixed messaging print

--- python_scripts/push_data_to_labstream.py
+++ python_scripts/push_data_to_labstream.py
@@ -30,10 +30,10 @@ try:
         buff += sys.stdin.read(1)
         if buff.startswith("relevant_entry"):
 	        if buff.endswith('\n'):
-	            print("Message received: "  +  str(buff[:-1].split(",")))
 	            frame_to_push = buff[:-1].split(",")
-	            counter = 0
 	            frame_to_push.pop(0)
+	            print("Message received: "  +  str(buff[:-1].split(",")))
+	            counter = 0
 	            for i in frame_to_push:
 	            	try:
 	            		frame_to_push[counter] = float(i)
