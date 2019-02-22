db33a87f1ab8bdc7a1b060e56ed54855713b3d1e
marc101101 <progamer101101@gmx.de>
2019-02-22 17:28:49 +0100

    remove first element

--- python_scripts/push_data_to_labstream.py
+++ python_scripts/push_data_to_labstream.py
@@ -33,7 +33,7 @@ try:
 	            print("Message received: "  +  str(buff[:-1].split(",")))
 	            frame_to_push = buff[:-1].split(",")
 	            counter = 0
-	            frame_to_push.remove(0)
+	            frame_to_push[0].remove()
 	            for i in frame_to_push:
 	            	try:
 	            		frame_to_push[counter] = float(i)
