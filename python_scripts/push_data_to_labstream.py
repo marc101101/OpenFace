3153272ea93770cfa0290aa6befc52a59e30354a
Markus Guder <progamer101101@gmx.de>
2019-02-26 11:05:31 +0100

    update video

--- python_scripts/push_data_to_labstream.py
+++ python_scripts/push_data_to_labstream.py
@@ -1,10 +1,27 @@
 import sys
 import time
 from pylsl import StreamInfo, StreamOutlet
+import cv2
+import numpy as np
 
 info = StreamInfo("client_name", 'FD', 17, 100, 'float32', 'gum11127_Openface')
 outlet = StreamOutlet(info)
 k = 0
+
+# Create a VideoCapture object
+cap = cv2.VideoCapture(0)
+
+# Check if camera opened successfully
+if (cap.isOpened() == False):
+    print("Unable to read camera feed")
+
+# Default resolutions of the frame are obtained.The default resolutions are system dependent.
+# We convert the resolutions from float to integer.
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
+out = cv2.VideoWriter('outvideo-' + str(time.time()) +'.mp4', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
 
 #std::cout << face_id << ", "
 #		<< frame_number << ", " 
@@ -27,6 +44,13 @@ k = 0
 try:
     buff = ''
     while True:
+        ret, frame = cap.read()
+        if ret == True:
+            try:
+                out.write(frame)
+                cv2.imshow('frame', frame)
+            except Exception as e:
+                print(e)
         buff += sys.stdin.read(1)
         if buff.startswith("relevant_entry"):
 	        if buff.endswith('\n'):
@@ -55,3 +79,9 @@ except KeyboardInterrupt:
    sys.stdout.flush()
    pass
 print("End of Log: "  + str(k))
+# When everything done, release the video capture and video write objects
+cap.release()
+out.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
