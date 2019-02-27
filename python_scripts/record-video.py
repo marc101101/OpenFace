bb824e6f478cf805ec0c5bfb0bc48ae090ab2e3d
Markus Guder <progamer101101@gmx.de>
2019-02-27 10:14:43 +0100

    update

--- python_scripts/record-video.py
+++ python_scripts/record-video.py
@@ -9,8 +9,8 @@ width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
 
 # Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use the lower case
-out = cv2.VideoWriter('output' + str(time.time()) + '.mp4', fourcc, 20.0, (width, height))
+fourcc = cv2.VideoWriter_fourcc('M','J','P','G') # Be sure to use the lower case
+out = cv2.VideoWriter('output' + str(time.time()) + '.avi', fourcc, 20.0, (width, height))
 
 while(cap.isOpened()):
     ret, frame = cap.read()
