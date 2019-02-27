0bcac95a2557a0a1f3342d18b16328340cb914dd
Markus Guder <progamer101101@gmx.de>
2019-02-27 10:11:02 +0100

    run

---
+++ python_scripts/record-video.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+import time
+
+cap = cv2.VideoCapture(0) # Capture video from camera
+
+# Get the width and height of frame
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use the lower case
+out = cv2.VideoWriter('output' + str(time.time()) + '.mp4', fourcc, 20.0, (width, height))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == True:
+        # write the flipped frame
+        out.write(frame)
+
+        #cv2.imshow('frame',frame)
+        if (cv2.waitKey(1) & 0xFF) == ord('q'): # Hit `q` to exit
+            break
+    else:
+        break
+
+# Release everything if job is finished
+out.release()
+cap.release()
+cv2.destroyAllWindows()
